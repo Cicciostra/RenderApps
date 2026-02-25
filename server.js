ebb77777cdb7381894f50cfe936a1491c9736348
Cicciostra <65898184+Cicciostra@users.noreply.github.com>
2026-02-25 17:51:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function ensureAuth(appName) {
 }
  
 app.get("/app1", ensureAuth("app1"), (req, res) => {
-  res.send("<head><meta charset='UTF-8'><title>APP1</title><style>body {margin: 0;padding: 0;font-family: Arial, sans-serif;background: linear-gradient(135deg, #ff9a9e, #fecfef, #fad0c4);color: #333;}header {background-color: #ff6f61;color: white;padding: 20px;text-align: center;font-size: 2em;letter-spacing: 1px;}.container {margin: 40px auto;width: 80%;background: white;padding: 20px 30px;border-radius: 12px;box-shadow: 0 4px 12px rgba(0,0,0,0.2);}h2 {color: #ff6f61;}button {padding: 12px 20px;font-size: 1em;background-color: #ff6f61;color: white;border: none;border-radius: 6px;cursor: pointer;transition: 0.3s;}button:hover {background-color: #e05850;}</style></head><body><header>🌈 Benvenuti in APP1 🌈</header><div class='container'><p>Questa è una pagina di prova di Francesco e Loredana.</p><p><a href='/app2'>Cliccami per andare all'App2!</a></p></br><p><a href='/app3'>Cliccami per andare all'App3!</a></p></div></body>");
+  res.send("<head><meta charset='UTF-8'><title>APP1</title><style>body {margin: 0;padding: 0;font-family: Arial, sans-serif;background: linear-gradient(135deg, #ff9a9e, #fecfef, #fad0c4);color: #333;}header {background-color: #ff6f61;color: white;padding: 20px;text-align: center;font-size: 2em;letter-spacing: 1px;}.container {margin: 40px auto;width: 80%;background: white;padding: 20px 30px;border-radius: 12px;box-shadow: 0 4px 12px rgba(0,0,0,0.2);}h2 {color: #ff6f61;}button {padding: 12px 20px;font-size: 1em;background-color: #ff6f61;color: white;border: none;border-radius: 6px;cursor: pointer;transition: 0.3s;}button:hover {background-color: #e05850;}</style></head><body><header>🌈 Benvenuti in APP1 🌈</header><div class='container'><p>Questa è una pagina di prova di Francesco e Loredana.</p><p><a href='/app2'>Cliccami per andare all'App2!</a></p><p><a href='/app3'>Cliccami per andare all'App3!</a></p></div></body>");
 });
  
 app.get("/app2", ensureAuth("app2"), (req, res) => {
@@ -64,6 +64,7 @@ app.post("/app3/acs",
  
 
 app.listen(process.env.PORT || 3000);
+
 
 
 
